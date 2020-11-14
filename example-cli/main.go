e9e25255cf4c8a4ed6823e9903b8324eb1c5d5e6
Eric Hulburd <ehulburd@rigetti.com>
2020-11-13 17:04:30 -0800

    debug auth commands

--- example-cli/main.go
+++ example-cli/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/rigetti/openapi-cli-generator/cli"
+	"github.com/rigetti/openapi-cli-generator/oauth"
 )
 
 func main() {
@@ -9,6 +10,23 @@ func main() {
 		AppName:   "example",
 		EnvPrefix: "EXAMPLE",
 		Version:   "1.0.0",
+	})
+
+	cli.UseAuth("bogus", &oauth.AuthCodeHandler{
+		ClientID:     "bogus01",
+		AuthorizeURL: "https://auth.qa.qcs.rigetti.com/v1/authorize",
+		TokenURL:     "https://auth.qa.qcs.rigetti.com/v1/token",
+		Keys:         []string{},
+		Params:       []string{},
+		Scopes:       []string{"offline_access"},
+	})
+	cli.UseAuth("", &oauth.AuthCodeHandler{
+		ClientID:     "bogus02",
+		AuthorizeURL: "https://auth.qa.qcs.rigetti.com/v1/authorize",
+		TokenURL:     "https://auth.qa.qcs.rigetti.com/v1/token",
+		Keys:         []string{},
+		Params:       []string{},
+		Scopes:       []string{"offline_access"},
 	})
 
 	openapiRegister(false)
